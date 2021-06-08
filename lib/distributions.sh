dd065989c4da35f2fbcafa4d4a0bcc1d14020f4c
debiangamer <xfce@optipc.lan>
2021-06-08 19:15:12 +0300

    patching

--- lib/distributions.sh
+++ lib/distributions.sh
@@ -381,7 +381,7 @@ install_common()
 	chroot "${SDCARD}" /bin/bash -c "systemctl --no-reload enable armbian-ramlog.service >/dev/null 2>&1"
 	chroot "${SDCARD}" /bin/bash -c "systemctl --no-reload enable armbian-resize-filesystem.service >/dev/null 2>&1"
 	chroot "${SDCARD}" /bin/bash -c "systemctl --no-reload enable armbian-hardware-monitor.service >/dev/null 2>&1"
-	chroot "${SDCARD}" /bin/bash -c "journalctl --vacuum-size=50M >/dev/null 2>&1"
+	chroot "${SDCARD}" /bin/bash -c "journalctl --vacuum-size=20M >/dev/null 2>&1"
 	# copy "first run automated config, optional user configured"
  	cp "${SRC}"/packages/bsp/armbian_first_run.txt.template "${SDCARD}"/boot/armbian_first_run.txt.template
 
